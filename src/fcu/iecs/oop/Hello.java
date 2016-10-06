087426bfc0af7181cef3a43064ee907bc9664d5f
d0441317 <jim.707039@gmail.com>
2016-10-06 23:22:42 +0800

    homework-1

--- src/fcu/iecs/oop/Hello.java
+++ src/fcu/iecs/oop/Hello.java
@@ -3,8 +3,10 @@ package fcu.iecs.oop;
 public class Hello {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Hello OOP") ;
+		
+		String lyrics = "Let her go! Let her go! Can't hold her back anymore";
+		
+		System.out.println(lyrics);
 	}
 
 }
